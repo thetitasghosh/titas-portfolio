12c82b43c9653d3ff129b9cf21dd4834ece99419
Titas Ghosh <rg814602@gmail.com>
2024-06-20 08:30:20 +0530

    add sitemap and robot file

---
+++ src/app/sitemap.ts
@@ -0,0 +1,23 @@
+import { MetadataRoute } from "next";
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: "",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+  ];
+}
