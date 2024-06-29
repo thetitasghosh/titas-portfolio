9c97688e5b1130448636e146f4d732b78fbf983e
Titas Ghosh <rg814602@gmail.com>
2024-06-29 18:25:33 +0530

    add  some local data to workData

---
+++ src/data/serviceData.js
@@ -0,0 +1,5 @@
+export const SERVICE = [
+    {id:1,img:"",servive:"",desq:""},
+    {id:2,img:"",servive:"",desq:""},
+    {id:3,img:"",servive:"",desq:""},
+]
--- src/data/workData.js
+++ src/data/workData.js
@@ -1,5 +1,7 @@
 import work1 from '../../public/'
 
 export const WORKS = [
-    {id:1,title:"",type:"",date:""}
+    {id:1,img:"",project:"personal",title:"",type:"",date:""},
+    {id:2,img:"",project:"personal",title:"",type:"",date:""},
+    {id:3,img:"",project:"client",title:"",type:"",date:""},
 ] 
