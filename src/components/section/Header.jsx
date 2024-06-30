7e197b20e42ecfcb78fd967e4c25b0ed79ee7a1a
Titas Ghosh <rg814602@gmail.com>
2024-06-30 13:22:38 +0530

    change header stack blend problem

--- src/components/section/Header.jsx
+++ src/components/section/Header.jsx
@@ -13,45 +13,46 @@ const Header = () => {
     console.log(isActive);
   });
   return (
-    <div className="redd fixed z-[999] flex h-16 w-screen items-center justify-between px-10 mix-blend-difference">
-      <div className="text-2xl text-white">
-        <h1 className="font-black">
-          <Link className="" href={"/"}>
-            TG
-          </Link>
-        </h1>
+    <>
+      <div className="redd fixed z-[999] flex h-16 w-screen items-center justify-between px-10 mix-blend-difference">
+        <div className="text-2xl text-white">
+          <h1 className="font-black">
+            <Link className="" href={"/"}>
+              TG
+            </Link>
+          </h1>
+        </div>
+        <div className="hidden h-10 items-center justify-evenly rounded-3xl border font-medium tablet:flex tablet:w-[25rem] laptop:w-[35rem]">
+          {nav.map((data, idx) => {
+            return (
+              <Route
+                isActive={isActive}
+                setIsActive={setIsActive}
+                key={idx}
+                data={data}
+              />
+            );
+          })}
+        </div>
+        <div
+          onClick={() => {
+            setIsActive(!isActive);
+          }}
+          className="z-[100] rounded-xl border px-2 mix-blend-difference tablet:hidden"
+        >
+          {isActive ? "close" : "menu"}
+        </div>
       </div>
-      <div className="hidden h-10 items-center justify-evenly rounded-3xl border font-medium tablet:flex tablet:w-[25rem] laptop:w-[35rem]">
-        {nav.map((data, idx) => {
-          return (
-            <Route
-              isActive={isActive}
-              setIsActive={setIsActive}
-              key={idx}
-              data={data}
-            />
-          );
-        })}
-      </div>
-      <div
-        onClick={() => {
-          setIsActive(!isActive);
-        }}
-        className="z-[100] rounded-xl border px-2 mix-blend-difference tablet:hidden"
-      >
-        {isActive ? "close" : "menu"}
-      </div>
-
-      {isActive && <Nav active={isActive} setActive={setIsActive} />}
-    </div>
+      <div>{isActive && <Nav active={isActive} setActive={setIsActive} />}</div>
+    </>
   );
 };
 
 function Nav({ active, setActive }) {
   return (
     <>
-      <div className="fixed  z-[99] right-3 top-3 flex h-96 w-[20rem] flex-col items-start justify-evenly gap-20 rounded-lg bg-white p-2 py-4 text-2xl text-black transition-transform">
-        <div className="flex  h-full w-full flex-col items-start justify-evenly px-20">
+      <div className="fixed right-3 top-3 z-[99] flex h-96 w-[20rem] flex-col items-start justify-evenly gap-20 rounded-lg bg-white p-2 py-4 text-2xl text-black transition-transform">
+        <div className="flex h-full w-full flex-col items-start justify-evenly px-20">
           {nav.map((data, idx) => {
             return (
               <Route
