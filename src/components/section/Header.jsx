7e11153832b9dc3dac21dd3702ed901054dd8a99
Titas Ghosh <rg814602@gmail.com>
2024-08-20 21:05:32 +0530

    add header

--- src/components/section/Header.jsx
+++ src/components/section/Header.jsx
@@ -1,7 +1,30 @@
 import React from "react";
-
+import {nav} from '@/data/navigation'
+import Link from "next/link";
 const Header = () => {
-  return <div className="w-full h-20"></div>;
+  return <div className="w-full mix-blend-exclusion redd h-14 flex items-center justify-between px-20 fixed top-0">
+    <Logo/>
+    <Nav nav={nav}/>
+  </div>;
 };
+
+function Logo (){
+  return (
+    <div className="redd">
+      <Link href={'/'}>Titas</Link>
+    </div>
+  )
+}
+function Nav({nav}){
+  return (
+    <div className="redd h-full flex items-center justify-center px-5 gap-5">
+        {
+          nav.map((data,i)=>{
+            return <div key={i}><Link href={data.route}>{data.label}</Link></div>
+          })
+        }
+    </div>
+  )
+}
 
 export default Header;
