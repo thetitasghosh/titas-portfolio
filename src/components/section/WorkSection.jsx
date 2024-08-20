81ec98e1a8f458df4f2e997b4a41f535559d583e
Titas Ghosh <rg814602@gmail.com>
2024-08-20 21:20:28 +0530

    ADD NEW WORK

--- src/components/section/WorkSection.jsx
+++ src/components/section/WorkSection.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-
+import { WORKS } from "@/data/workData";
+import Card from "@/components/ui/Card";
 const WorkSection = () => {
   return (
     <div className="red flex h-screen w-full flex-col items-center justify-center p-2">
       <div>
         <h1>Work Section</h1>
       </div>
-      <div className="w-full h-full">
-
+      <div className="flex h-full w-full items-center justify-center gap-10">
+        {WORKS.map((data, i) => {
+          return <Card key={i} data={data} />;
+        })}
       </div>
     </div>
   );
--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -1,9 +1,13 @@
+import Image from 'next/image';
 import React from 'react';
 
-const Card = () => {
+const Card = ({data}) => {
   return (
-    <div className='w-96 h-[30rem] red rounded-md flex flex-col items-center justify-center'>
-        
+    <div className='w-96 h-96 py-5 p-5 red rounded-md flex flex-col items-center justify-center'>
+        <div className='w-full h-full'>
+          <Image src={data.img} width={1000} height={500} className='w-full' alt='Titas Portfolio Projects'/>
+        </div>
+        <h1>{data.title}</h1>
     </div>
   );
 }
