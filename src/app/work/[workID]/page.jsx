6682d8f5af0233e68c09f5e63d111241a08780ad
Titas Ghosh <rg814602@gmail.com>
2024-09-10 20:10:36 +0530

    add dynamic meta data

--- src/app/work/[workID]/page.jsx
+++ src/app/work/[workID]/page.jsx
@@ -1,10 +1,27 @@
 import { WORKS } from "@/data/workData";
+import Image from "next/image";
 
-export async function generateStaticParams() {
-  // Generate static parameters from local data
-  return WORKS.map((itm) => ({ workID: itm.slug }));
+export async function generateMetadata({ params }) {
+  const { workID } = params;
+
+  // Find the specific project by slug (workID)
+  const project = WORKS.find((item) => item.slug === workID);
+
+  // Return title and other metadata only if the project exists
+  if (project) {
+    return {
+      title: project.title,
+      description: project.description || "Project description here",
+      // You can also add more metadata such as open graph or twitter card metadata
+    };
+  }
+
+  // Fallback metadata if no project is found
+  return {
+    title: "Project Not Found",
+    description: "This project does not exist in our database.",
+  };
 }
-
 export default function ItemPage({ params }) {
   const workitem = WORKS.find((item) => item.slug === params.workID);
 
@@ -13,9 +30,20 @@ export default function ItemPage({ params }) {
   }
 
   return (
-    <div className="flex h-screen w-full bg-black flex-col items-center justify-center">
-      <h1>Title:{workitem.title}</h1>
-      <p>Type:{workitem.type}</p>
+    <div className="flex h-screen w-full flex-col items-center justify-center bg-black">
+      <div className="redd z-50 flex h-full w-full justify-between px-5 pb-10 pt-52 mix-blend-exclusion desktop:items-end desktop:pt-0 desktop:text-5xl">
+        <h1>{workitem.title}</h1>
+        <p>{workitem.type}</p>
+      </div>
+      <div className="redd absolute h-full w-full">
+        <Image
+          src={workitem.img}
+          alt="projects"
+          width={1000}
+          height={1000}
+          className="h-full w-full object-contain"
+        />
+      </div>
     </div>
   );
 }
