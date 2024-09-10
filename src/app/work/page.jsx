0ce7782cd98356ad0738e9fd20d10ea896ab0291
Titas Ghosh <rg814602@gmail.com>
2024-09-10 20:20:33 +0530

    add transition

--- src/app/work/page.jsx
+++ src/app/work/page.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { WORKS } from "@/data/workData";
-import Link from "next/link";
+// import Link from "next/link";
+import { Link } from "next-view-transitions";
 
 export const metadata = {
   title: "Works",
