96f08d5a8165d39bb907bfd823e44a52e9fd0dfb
Titas Ghosh <rg814602@gmail.com>
2024-09-21 08:47:44 +0530

    add new route "View"

---
+++ src/app/view/page.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-3 bg-black">
+      <h1 className="text-2xl">View is Coming Soon</h1>
+      <p className="w-96 text-justify">
+        People&apos;s views significantly influence their decision-making
+        processes in everyday life. These influences can stem from various
+        sources, including personal values, societal norms, and the opinions of
+        others. Understanding these influences is crucial for making conscious
+        and informed choices.
+      </p>
+    </div>
+  );
+};
+
+export default page;
