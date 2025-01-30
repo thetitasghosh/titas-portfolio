8c0272ca0b618106c5af62f1368c82062120fe6b
thetitasghosh <rg814602@gmail.com>
2025-01-30 19:38:18 +0530

    update middlware matcher

--- middleware.ts
+++ middleware.ts
@@ -22,5 +22,13 @@ export async function middleware(req: NextRequest) {
 
 // Apply middleware only to pages (not API routes)
 export const config = {
-  matcher: "/:path*",
+  /*
+   * Match all request paths except for the ones starting with:
+   * - api (API routes)
+   * - _next/static (static files)
+   * - _next/image (image optimization files)
+   * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+   */
+  matcher:
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
 };
