914eb85b5e7a53ed3eaf2451df65a10d4f244749
thetitasghosh <rg814602@gmail.com>
2025-06-21 12:43:22 +0530

    update hero text

--- src/components/sections/hero-section.jsx
+++ src/components/sections/hero-section.jsx
@@ -7,9 +7,9 @@ import { Avatar, AvatarImage } from "@/components/ui/avatar";
 const HeroSection = () => {
   return (
     <div className="redd my-5 flex h-[65vh] w-full flex-col items-center justify-center gap-10 border-b text-neutral-800 dark:border-neutral-800">
-      <h1 className="text-center text-2xl font-medium desktop:text-4xl tablet:w-[65%] desktop:w-[55%]">
-        Titas here — I build clean, fast, and purposeful digital products.
-        Developer-first. Founder-minded.
+      <h1 className="text-center text-2xl font-medium tablet:w-[65%] desktop:w-[55%] desktop:text-4xl">
+        I&apos;m Titas — a frontend developer building clean, fast, and
+        meaningful experiences.
       </h1>
     </div>
   );
