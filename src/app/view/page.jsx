fc8cb825f089dff74ca94178731117055765acf8
thetitasghosh <rg814602@gmail.com>
2025-06-22 10:43:16 +0530

    update view

--- src/app/view/page.jsx
+++ src/app/view/page.jsx
@@ -5,11 +5,14 @@ const ViewPage = () => {
     <div className="flex h-screen w-full flex-col items-center justify-center gap-3">
       <h1 className="text-2xl">View is Coming Soon</h1>
       <p className="w-96 text-justify">
-        People&apos;s views significantly influence their decision-making
-        processes in everyday life. These influences can stem from various
-        sources, including personal values, societal norms, and the opinions of
-        others. Understanding these influences is crucial for making conscious
-        and informed choices.
+        The way we see the world shapes the way we move through it. Our
+        perspectives are shaped by personal values, society, and the voices
+        around us. But clarity comes when we pause, question, and choose what
+        truly aligns.
+      </p>
+      <p className="w-96 text-justify">
+        This space will soon be home to reflections, mental models, and thoughts
+        on building, thinking, and living with intention.
       </p>
     </div>
   );
