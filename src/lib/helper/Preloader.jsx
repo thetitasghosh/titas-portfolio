619fe41e69d71db18cec8d0fdfa0260cb8bb6804
thetitasghosh <rg814602@gmail.com>
2025-06-22 22:21:46 +0530

    fix

--- src/lib/helper/Preloader.jsx
+++ src/lib/helper/Preloader.jsx
@@ -4,7 +4,7 @@ import React from "react";
 import { PreloaderSildeUp } from "@/lib/anim/animation";
 import SVG from "@/lib/helper/SVG";
 const Preloader = () => {
-  const completed = sessionStorage.getItem("PreloaderCompleted");
+  // const completed = sessionStorage.getItem("PreloaderCompleted");
   return (
     <motion.div
       variants={PreloaderSildeUp}
@@ -13,7 +13,7 @@ const Preloader = () => {
       className="fixed right-0 top-0 z-[9999] flex h-screen w-full items-center justify-center bg-white stroke-black dark:bg-black dark:[&_svg]:stroke-white"
     >
       <div className="redd flex size-full items-center justify-center">
-        {completed && <SVG />}
+        <SVG />
       </div>
     </motion.div>
   );
